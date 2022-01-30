626c4c524d884b3e78f80303c340a5cb27deb64f
Ronmi Ren <ronmi.ren@gmail.com>
2022-01-31 01:59:21 +0800

    主程式加上 logging
    `tracing` 用起來十分爽快 (下一個 commit 就會看到)，但是設定起來頗麻煩。
    如果沒有一定要用 local time 記錄的話還好，想要用 local time 就會像現在
    這樣，引入一堆 crate 還要轉來轉去。
    
    這是因為 tracing 是用 time crate 來處理時間的，但是 time crate 偵測本
    地時區的程式比較原始，不能保證會有答案，只好使用 chrono crate 的方式來
    取得本地時區，再轉換成 time crate 的格式。

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use chrono::offset::Local;
 use clap::Parser;
 use grpc_chatroom::{proto::chat_server::ChatServer, Server};
 use sqlx::Pool;
 use std::net::SocketAddr;
+use time::macros::format_description;
+use time::UtcOffset;
+use tracing::Level;
+use tracing_subscriber::fmt::time::OffsetTime;
 
 #[derive(Parser, Debug)]
 #[clap()]
@@ -10,11 +15,32 @@ struct Args {
     bind: String,
     #[clap(short, long)]
     database: String,
+    /// log level
+    #[clap(short, long, default_value = "info")]
+    level: Level,
 }
 
 #[tokio::main]
 async fn main() {
     let app = Args::parse();
+
+    // init log
+    let offset = Local::now().offset().local_minus_utc();
+    let offset = UtcOffset::from_whole_seconds(offset).unwrap();
+    tracing_subscriber::fmt()
+        .with_ansi(true)
+        .with_level(true)
+        .with_line_number(true)
+        .with_file(true)
+        .with_max_level(app.level)
+        .with_timer(OffsetTime::new(
+            offset,
+            format_description!(
+                "[year]-[month]-[day] [hour]:[minute]:[second] [offset_hour sign:mandatory]:00"
+            ),
+        ))
+        .pretty()
+        .init();
 
     let bind: SocketAddr = app.bind.as_str().parse().unwrap();
 
@@ -32,6 +58,7 @@ async fn main() {
 
     let signal = async_ctrlc::CtrlC::new().unwrap();
 
+    tracing::info!(?bind, %offset, "啟動 rpc server");
     tonic::transport::server::Server::builder()
         .accept_http1(true)
         .add_service(web.enable(svc))
