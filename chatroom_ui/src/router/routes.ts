ce631107a2aa980a00df66729fd27a6483b34b7b
Ronmi Ren <ronmi.ren@gmail.com>
2022-01-31 20:44:09 +0800

    準備所有的 component 和 page
    把多餘的拿掉，順便加上要用的
    
    - page/Index: 首頁，要求用戶輸入名稱
    - page/Main: 主要頁面，顯示訊息和輸入訊息的地方
    - component/MessageList: 顯示訊息的 component

--- chatroom_ui/src/router/routes.ts
+++ chatroom_ui/src/router/routes.ts
@@ -4,7 +4,10 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/Index.vue') }],
+    children: [
+      { path: '', component: () => import('pages/Index.vue') },
+      { path: 'chat', component: () => import('pages/Main.vue') },
+    ],
   },
 
   // Always leave this as last one,
