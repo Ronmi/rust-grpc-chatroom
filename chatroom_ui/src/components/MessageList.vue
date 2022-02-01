ef872d4652037ba4a74238bfdb31c5b003b93d42
Ronmi Ren <ronmi.ren@gmail.com>
2022-02-01 17:11:44 +0800

    實作訊息介面
    server
    ======
    
    測試這個頁面需要先啟動 grpc server
    
    - 回到專案根目錄
    - `docker-compose up -d db` 啟動 db
    - `cargo run -- -b 0.0.0.0:50051 -d 'postgresql://postgres:qwer1234@localhost:5432/chatroom'` 啟動 server
    
    MessageList
    ===========
    
    composition 定義 props 的方式是透過 `defineProps` 處理，不過這邊因為用
    不到，所以沒有接回傳值。其他都是基本 vue 操作，不再贅述。
    
    pages/Main
    ==========
    
    grpc 的部份就先用寫死的位址，之後再調整。底下的程式是啟用瀏覽器的
    grpc-web extenstion 必要的程式，詳見 [這裡](https://github.com/SafetyCulture/grpc-web-devtools)
    
    grpc streamming 是透過 `on` 來註冊 event handler 的，作為示範只有處理
    了兩個: `data` 是有資料來，`error` 是發生錯誤。這個處理不是正確的處理
    方式，後續修改就交由讀者自行發揮。
    
    結語
    ====
    
    簡單的 chatroom 已經完成，後續有幾個常見的方向可以進行
    
    - deploy 出去
      * 先把 grpc 位址修改好
      * 執行 `quasar build`
      * 把 `dist/` 裡的東西丟到 webserver 上
      * 回專案根目錄
      * 執行 `cargo build --release` 把檔案丟到 linux 機上
      * 處理 linux 機的上 db
      * 執行 server 檔
    - 把上述 deploy 動作改成用 CD 處理
    - 加上註冊及登入
    - 修改 `pages/Main` 改成抓 json 檔決定 grpc 位址
    - 調整 server 程式，直接用 server 當成 webserver 來供應 ui

--- chatroom_ui/src/components/MessageList.vue
+++ chatroom_ui/src/components/MessageList.vue
@@ -1,10 +1,39 @@
 <template>
-  <div>My component</div>
+  <q-list>
+    <q-item v-for="(msg, idx) in items" :key="idx">
+      <q-item-section top>
+        <div class="row justify-start content-top items-top">
+          <q-chip dense size="sm" color="primary" outline square>{{
+            msg.name
+          }}</q-chip>
+          <div>{{ msg.msg }}</div>
+        </div>
+      </q-item-section>
+      <q-item-section side>
+        {{ fmtTime(msg.createAt) }}
+      </q-item-section>
+    </q-item>
+  </q-list>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  // name: 'ComponentName'
-})
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+import { Message } from 'src/proto/chat_pb';
+
+//////////////// props
+defineProps<{ items: Message.AsObject[] }>();
+
+//////////////// 時間顯示的處理
+function _pad(n: number): string {
+  const ret = String(n);
+  if (ret.length < 2) {
+    return '0'.repeat(2 - ret.length) + ret;
+  }
+
+  return ret;
+}
+function fmtTime(ts: number): string {
+  const d = new Date(ts * 1000);
+  return _pad(d.getHours()) + ':' + _pad(d.getSeconds());
+}
 </script>
--- chatroom_ui/src/pages/Main.vue
+++ chatroom_ui/src/pages/Main.vue
@@ -1,12 +1,72 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <q-card flat>
+      <q-card-section>
+        <message-list :items="msgs" />
+      </q-card-section>
+
+      <q-card-section>
+        <q-form @submit.prevent="say" class="row q-gutter-xs">
+          <q-input
+            v-model="myMsg"
+            color="primary"
+            class="col"
+            :prefix="name + ':'"
+            autofocus
+            dense
+            hide-hint
+          />
+          <q-btn type="submit" color="primary" class="col-auto" label="送出" />
+        </q-form>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  // name: 'PageName'
-})
+<script lang="ts" setup>
+import { ref, computed } from 'vue';
+import { useStore } from 'src/store';
+import MessageList from 'src/components/MessageList.vue';
+import { JoinReq, Message, NewMsgReq, SayReq } from 'src/proto/chat_pb';
+import { ChatClient } from 'src/proto/ChatServiceClientPb';
+
+//////////////// 共用資料
+const _store = useStore();
+const name = computed(() => _store.state.name);
+const _api = new ChatClient('http://127.0.0.1:50051');
+
+(function () {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
+  const x = (window as any).__GRPCWEB_DEVTOOLS__;
+  if (x) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    x([_api]);
+  }
+})();
+
+//////////////// 先抓舊資料
+const msgs = ref<Message.AsObject[]>([]);
+async function _boot() {
+  const res = await _api.join(new JoinReq(), null);
+  msgs.value = res.getMsgsList().map((v) => v.toObject());
+}
+_boot().then(null, null);
+
+//////////////// 訂閱即時訊息通知
+function _subscribe() {
+  _api
+    .newMsg(new NewMsgReq())
+    .on('data', (v: Message) => {
+      msgs.value.push(v.toObject());
+    })
+    .on('error', _subscribe);
+}
+_subscribe();
+
+//////////////// 處理發言
+const myMsg = ref('');
+async function say() {
+  await _api.say(new SayReq().setName(name.value).setMsg(myMsg.value), null);
+  myMsg.value = '';
+}
 </script>
